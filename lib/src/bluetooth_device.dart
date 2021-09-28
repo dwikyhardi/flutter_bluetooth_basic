47369f6bdc35cde7da60d85072e8699eeb103877
Dwiky Hardiansah <dwikyhardiansah93@gmail.com>
2021-09-29 03:31:01 +0700

    fixing bluetoothDevice

--- lib/src/bluetooth_device.dart
+++ lib/src/bluetooth_device.dart
@@ -4,14 +4,16 @@ part 'bluetooth_device.g.dart';
 
 @JsonSerializable(includeIfNull: false)
 class BluetoothDevice {
-  BluetoothDevice();
+  BluetoothDevice(
+      {this.name, this.address, this.type = 0, this.connected = false});
 
   String? name;
   String? address;
-  int type = 0;
-  bool connected = false;
+  int type;
+  bool connected;
 
   factory BluetoothDevice.fromJson(Map<String, dynamic> json) =>
       _$BluetoothDeviceFromJson(json);
+
   Map<String, dynamic> toJson() => _$BluetoothDeviceToJson(this);
 }
